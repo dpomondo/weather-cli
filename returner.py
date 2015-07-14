d42518b26cc3be0dcacecf12e7a651ada60fb692
lhadpoyesac <dpomondo@gmail.com>
2015-07-13 21:25:26 -0600

    1. returner should deal with new shelve files better 2. tried to make opening shelve files less indiscriminate; may not have suceeded

--- returner.py
+++ returner.py
@@ -11,9 +11,13 @@ home_dir = '/usr/self/weather/'
 shelve_file = home_dir + time.strftime("%d%b%y") + "_weather"
 weather_db = shelve.open(shelve_file)
 
-ob_ep = int(weather_db['current_response'][
-    'current_observation']['observation_epoch'])
-if ob_ep + time_out < time.time():
+# if the weather_db shelve file has only just been initialized, OR
+# the time since the last request plus time out length is less than
+# the current time, call getter.update()
+if ('current_response' not in weather_db or
+        (int(weather_db['current_response']
+                       ['current_observation']
+                       ['observation_epoch']) + time_out < time.time())):
     if '/usr/self/bin' not in sys.path:
         sys.path.append('/usr/self/bin')
     weather_db.close()
