11a9fafb17471eb57bccae41901cb1f140a2f054
lhadpoyesac <dpomondo@gmail.com>
2015-07-19 09:32:20 -0600

    refactored the mess in returner, now less likely to fail if there is no saved server call

--- returner.py
+++ returner.py
@@ -3,27 +3,56 @@
 
 import json
 import sys
+import os
 import time
 
 time_out = 600
-
 home_dir = '/usr/self/weather/'
 shelve_file = home_dir + time.strftime("%d%b%y") + "_weather"
-with open(shelve_file + ".json", "r") as infile:
-    current = json.load(infile)
-# weather_db = shelve.open(shelve_file)
+today_current = shelve_file + ".json"
 
-ob_ep = int(current['current_observation']['observation_epoch'])
-if ob_ep + time_out < time.time():
+
+def call_getter():
     if '/usr/self/bin' not in sys.path:
         sys.path.append('/usr/self/bin')
     import shelve
     import getter
+    # need to move this `shelve.open` call to the update function...
     weather_db = shelve.open(shelve_file)
     getter.update(weather_db, check_time=False)
     weather_db.close()
-temp = current['current_observation']['temp_f']
-# weather_db.close()
-out = "Current Temp: {}".format(temp)
 
-sys.stdout.write(out)
+
+def open_current_file():
+    with open(today_current, "r") as infile:
+        temp = json.load(infile)
+    return temp
+
+
+def get_current_time(temp):
+    return int(temp['current_observation']['observation_epoch'])
+
+
+def temp_current_get(temp):
+    return temp['current_observation']['temp_f']
+
+
+def main():
+    if not os.path.isfile(today_current):
+        call_getter()
+
+    current = open_current_file()
+    ob_ep = get_current_time(current)
+
+    if ob_ep + time_out < time.time():
+        call_getter()
+
+    temp = temp_current_get(current)
+
+    out = "Current Temp: {}".format(temp)
+
+    sys.stdout.write(out)
+
+
+if __name__ == "__main__":
+    main()
