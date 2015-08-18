b2184e40c3f3ec91124085c6f7501ca7a2ef1445
lhadpoyesac <dpomondo@gmail.com>
2015-08-17 20:41:16 -0600

    refactored print_hourly sunrise/sunset colr formatting into seperate funcs

--- printers/print_hourly.py
+++ printers/print_hourly.py
@@ -44,6 +44,7 @@ def format_bar_hour(weat_hour, COLOR, zero_hour, sunrise, sunset):
     for r in [(('temp', 'english'), COLOR.hot, COLOR.cool),
               (('sky', ), COLOR.hot, COLOR.cool),
               (('pop', ), COLOR.hot, COLOR.cool),
+              (('wspd', 'english'), COLOR.hot, COLOR.cool)
               ]:
         target = weat_hour
         currs = zero_hour
@@ -51,36 +52,66 @@ def format_bar_hour(weat_hour, COLOR, zero_hour, sunrise, sunset):
             target = target[z]
             currs = currs[z]
         res.append('{}{:^6}{}'.format(r[1] if target > currs else
-                                      r[2] if target < currs else
-                                      COLOR.temp, target, COLOR.clear))
-    rise_dif = int(weat_hour['FCTTIME']['hour']) - int(sunrise[0])
-    set_dif = int(weat_hour['FCTTIME']['hour']) - int(sunset[0])
-    res.append("{}{:^6}{}".format(COLOR.dusk if set_dif >= -1 and set_dif < 1
-                                  else
-                                  COLOR.dawn if rise_dif >= -1 and rise_dif < 1
-                                  else
-                                  COLOR.night if rise_dif * set_dif > 0
-                                  else COLOR.day,
-                                  "{}:{}".format(*sunrise) if rise_dif == 0
-                                  else
-                                  "{}:{}".format(*sunset) if set_dif == 0
-                                  else "",
-                                  COLOR.clear))
+                                      r[2] if target < currs else COLOR.temp,
+                                      target, COLOR.clear))
+    res.append("{}{:^6}{}".format(
+        sunrise_sunset_color(weat_hour['FCTTIME']['hour'],
+                             sunrise, sunset, COLOR),
+        sunrise_sunset_time(weat_hour['FCTTIME']['hour'], sunrise, sunset),
+        COLOR.clear))
     res.append("{:^6}".format("{}:{}".format(weat_hour['FCTTIME']['hour'],
                                              weat_hour['FCTTIME']['min'])))
-
     return res
+
+
+def sunrise_sunset_color(hour, sunrise, sunset, CLR):
+    """ Returns a color code depending on distance from sunrise/sunset
+
+        args:   hour: string representing an hour ('0' to '23')
+                sunrise: tuple of two strings, representing hour and minute
+                sunset: tuple of two strings, representing hour and minute
+                CLR: Color class object created by utilities.get_colors()
+
+        returns:    Color class attribute (a printable color escape code)
+                    depending on the difference between the given hour and the
+                    hour of sunrise/sunset.
+    """
+    set_dif = int(hour) - int(sunset[0])
+    rise_dif = int(hour) - int(sunrise[0])
+    return (CLR.dusk if set_dif > -1 and set_dif <= 1 else
+            CLR.dawn if rise_dif >= -1 and rise_dif < 1 else
+            CLR.night if rise_dif * set_dif > 0 else
+            CLR.day)
+
+
+def sunrise_sunset_time(hour, sunrise, sunset):
+    """ Determines whether a given hour includes sunrise or sunset.
+
+        args:   hour: string indicating an hour ('0' to '23')
+                sunrise: tuple of two strings, hour and minute
+                sunset: tuple including two strings, hour and minute
+
+        Returns:    empty string if the given hour does NOT include rise/set,
+                    formatted sunrise time if given hour includes sunrise,
+                    formatted sunset time if given hour includes sunset
+        """
+    return ("{}:{}".format(*sunrise) if int(hour) - int(sunrise[0]) == 0
+            else
+            "{}:{}".format(*sunset) if int(hour) - int(sunset[0]) == 0
+            else "")
 
 
 def hourly_by_bars(hourly_wdb, width, height, sun_wdb):
     res = [[]]
     fins = []
     COLORS = printers.utilities.get_colors()
-    _keys = ["Temp", "Cloud %", "Precip Chance", "Sunrise/set", "Time"]
+    _keys = ["Temp", "Cloud %", "Precip Chance", "Wind speed",
+             "Sunrise/set", "Time"]
     for k in _keys:
         res[0].append("{:>{width}}: ".format(k,
                       width=max(list(len(z) for z in _keys))))
-    for i in range((width - max(list(len(z) for z in res[0])))//6):
+    # for i in range((width - max(list(len(z) for z in res[0])))//6):
+    for i in range((width - len(res[0][0])) // 6):
         res.append(format_bar_hour(hourly_wdb[i], COLORS, hourly_wdb[0],
                    (sun_wdb['sunrise']['hour'], sun_wdb['sunrise']['minute']),
                    (sun_wdb['sunset']['hour'], sun_wdb['sunset']['minute'])))
@@ -95,11 +126,11 @@ def main():
     import returner
     now = returner.main()
     print("Testing printing by lines:")
-    nerd = print_hourly(now['hourly_forecast'], frmt='lines')
+    nerd = print_hourly(now['hourly_forecast'], None, frmt='lines')
     for lin in nerd:
         print(lin)
     print("Testing bars...")
-    nerd = print_hourly(now['hourly_forecast'], frmt='bars')
+    nerd = print_hourly(now['hourly_forecast'], now['sun_phase'], frmt='bars')
     for lin in nerd:
         print(lin)
 
